4063e74f113cd3fec642bb62300708d206cd5149
renanuness <renan1504@gmail.com>
2024-09-24 00:42:14 -0300

    opções de visualização de produtos

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -2,7 +2,6 @@
 
 import { useContext, useEffect, useState } from "react";
 import ProductCard from "../../components/productCard";
-import ProductFilter from "../../components/productFilter";
 import { getProducts, filterProducts } from "../../services/services";
 import { useNavigate } from "react-router-dom";
 
@@ -11,33 +10,40 @@ import  {useAuth}  from "../../contexts/authContext";
 export default function Products(){
     const navigate = useNavigate();
     const {user} = useAuth();
-    const pageSize = 30;
     const [products, setProducts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [canLoad, setCanLoad] = useState(true);
-    const [saved, setSaved] = useState([]);
+    const [view, setView] = useState("grid");
+    const [style, setStyle] = useState("flex flex-wrap gap-2 p-4 justify-center");
 
     console.log(user.token);
     useEffect(()=>{
-        getProducts(user.token, pageSize).then(res=>{
+        getProducts(user.token).then(res=>{
             console.log(res);
             setProducts(res);
         });
     }, []);
 
+    useEffect(()=>{
+        let s = view == 'grid' ?
+            "flex flex-row flex-wrap gap-2 p-4 justify-center" :
+            "flex flex-col gap-2 p-4 justify-center";
+
+            setStyle(s);
+    }, [view]);
+
+    function activeButton(value){
+        return (view == value ? 'bg-green-600': '');
+    }
     return (
         <div className="flex-col flex items-center">
-            <ProductFilter filter={(v)=>filter(v)} />
-            <div className="flex flex-wrap gap-2 p-4 justify-center">
+            <div className="p-2 flex gap-2">
+                <button className={"p-2 border "+ activeButton('grid') } onClick={()=>setView("grid")}>Grade</button>
+                <button className={"p-2 border "+ activeButton('list') } onClick={()=>setView("list")}>Lista</button>
+            </div>
+            <div className={style}>
             {products.map(product =>
-                <ProductCard key={product.id} product={product} saved={()=>{saved.includes(product.id)}} productDetail={(id)=>navigate('/productDetail/'+id)}/>
+                <ProductCard key={product.id} product={product} productDetail={(id)=>navigate('/productDetail/'+id)}/>
             )}
             </div>
-            { 
-                canLoad ?
-                <button className="bg-green-400 rounded-md w-44 mb-4" onClick={(e)=>loadMore(e)}>Carregar mais</button>
-                : ''
-            }
         </div>
     )
 }
